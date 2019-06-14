034efbbdff1ab8759bf51a8c16b014116ffac7dc
Ruzanov Sergey <SRuzanov84@gmail.com>
2019-06-15 03:06:36 +0500

    Добавьте файлы проекта.

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputA;
+            double y = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Введите число:");
+                while (true)
+                {
+                    if (double.TryParse(Console.ReadLine(), out inputA))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный ввод. Введите корректные данные:");
+                    }
+                }
+
+                if (inputA < -1)
+                {
+                    y = -1 / (inputA * inputA);
+                }
+                else if (inputA >= -1 && inputA <= 2)
+                {
+                    y = inputA * inputA;
+                }
+                else if (inputA > 2)
+                {
+                    y = 4;
+                }
+
+                Console.WriteLine($"f({inputA}) = {y}");
+            }
+        }
+    }
+}
